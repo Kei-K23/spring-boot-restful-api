fea5f445f3964d607d6a0903488b86a33028d17e
Arkar Min <arkarmin@Arkars-MacBook-Air.local>
2025-04-08 22:21:38 +0630

    init commit

---
+++ src/test/java/com/github/keik23/spring_boot_restful_api/SpringBootRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.keik23.spring_boot_restful_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
